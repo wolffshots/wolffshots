fefe496f33136ebc66615dff1dbe4cdf8ba743a5
jadon <16850875+wolffshots@users.noreply.github.com>
2021-02-24 21:22:57 +0200

    removed p from encoder

--- src/pages/Projects/Encoder/Encoder.tsx
+++ src/pages/Projects/Encoder/Encoder.tsx
@@ -2,7 +2,6 @@ import Layout from "../../../components/Layout/Layout";
 const Encoder = () => {
   return (
     <Layout>
-      <p>about</p>
       <div>flesh out encoder app here</div>
     </Layout>
   );
