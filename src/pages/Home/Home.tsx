36ea05b76627d7ce1a3fed5b4778b884e346a87f
jadon <16850875+wolffshots@users.noreply.github.com>
2021-02-24 18:20:28 +0200

    added some theming to more of home and started home copy

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,16 @@
-import logo from "../../logo.svg";
 import Layout from "../../components/Layout/Layout";
 
 const About = () => {
   return (
     <Layout>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
+      <p>Placeholder paragraph for explanation about me</p>
       <a
-        className="App-link"
-        href="https://reactjs.org"
+        className="dark:text-blue-600 text-black"
+        href="https://www.github.com/wolffshots"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Learn React
+        View my GitHub
       </a>
     </Layout>
   );
