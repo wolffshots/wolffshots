98628993138bad3c9c6126ec61eaa75f74e4cbc4
jadon <16850875+wolffshots@users.noreply.github.com>
2021-02-24 18:35:11 +0200

    fixed colours of link

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <Layout>
       <p>Placeholder paragraph for explanation about me</p>
       <a
-        className="dark:text-blue-600 text-black"
+        className="dark:text-blue-300 text-green-500"
         href="https://www.github.com/wolffshots"
         target="_blank"
         rel="noopener noreferrer"
